23e2fed8f319b5d3da7ffee16b2378e156615681
Dilan Martinez <57841420+dmartinez16@users.noreply.github.com>
2023-01-22 14:13:57 -0500

    Add files via upload

---
+++ Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVC_CRUD.Models;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public DateTime? Fecha { get; set; }
+
+    public string? Clave { get; set; }
+}
